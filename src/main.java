e2b943e79456dbc3770d63f0695afbb27b6e3064
ullllosta <lizayakushova172@gmail.com>
2024-03-20 09:55:23 +0300

    changed main.java

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class main {
     public static void main(String[] args) {
-
+        System.out.println("postav'te pyat' pojaluista");
     }
 }
 
